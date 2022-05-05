d2fd2c818bd16938eec07869156a761b063bb619
Tom-the-Bomb <thello522@gmail.com>
2022-05-05 17:24:17 -0400

    generic typing for contexvt

--- bot/context.py
+++ bot/context.py
@@ -8,9 +8,8 @@ from discord.ext import commands
 if TYPE_CHECKING:
     from .bot import MathBot
 
-class MathContext(commands.Context):
-    bot: MathBot
-
+class MathContext(commands.Context['MathBot']):
+    
     async def reply(self, content: Any = None, **kwargs) -> discord.Message:
         mention_author = kwargs.pop('mention_author', False)
         return await super().reply(content, mention_author=mention_author, **kwargs)
